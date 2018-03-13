8996bedccab209910319136b12592b87851f84c3
soulwawa <soulwawa85@gmail.com>
2018-03-13 18:41:53 +0900

    DBUtil 생성

---
+++ javadev/SafeForYou/src/util/DBUtil.java
@@ -0,0 +1,66 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DBUtil {
+	public static Connection getConnection() {
+		try {
+			//문자열로 전달되는 클래스가 존재하는 클래스를 메모리로 로드하는 역활.
+			Class.forName("com.mysql.jdbc.Driver");
+			return DriverManager.getConnection("jdbc:mysql://10.1.41.26:3306/mydb", "root", "1234");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static void close(PreparedStatement stmt, Connection conn) {
+		try {
+			if(stmt != null)
+				stmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			stmt = null;
+		}
+		try {
+			if(conn != null)
+				conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			conn = null;
+		}
+	}
+
+	public static void close(ResultSet rst, PreparedStatement stmt, Connection conn) {
+		try {
+			if(rst != null)
+				rst.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			rst = null;
+		}
+		try {
+			if(stmt != null)
+				stmt.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			stmt = null;
+		}
+		try {
+			if(conn != null)
+				conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			conn = null;
+		}
+	}
+}
+
