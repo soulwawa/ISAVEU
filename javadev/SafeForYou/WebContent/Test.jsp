5eb57406a0319eeea02b23e3fe3a33552ccc2eab
soulwawa <soulwawa85@gmail.com>
2018-03-14 15:42:32 +0900

    SERVER - TEST페이지 생성, TempVO 생성

--- javadev/SafeForYou/WebContent/Test.jsp
+++ javadev/SafeForYou/WebContent/Test.jsp
@@ -4,10 +4,10 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+<title>TEST</title>
 </head>
 <body>
-<h1>Hello SFY</h1>
-<h2>GIT 동기화 TEST23</h2>
+<h1>TEST 페이지 입니다.</h1>
+
 </body>
 </html>
--- javadev/SafeForYou/src/model/TempVO.java
+++ javadev/SafeForYou/src/model/TempVO.java
@@ -7,28 +7,49 @@ public class TempVO implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private int id;
-	private double temp;
-	private String time;
-
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public double getTemp() {
-		return temp;
-	}
-	public void setTemp(double temp) {
-		this.temp = temp;
-	}
-	public String getTime() {
-		return time;
-	}
-	public void setTime(String time) {
-		this.time = time;
+	private String num;
+	private String loc;
+	private String type;
+	private String temperature;
+	private String idate;
+	
+	public String getLoc() {
+		return loc;
 	}
 
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
 
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getTemperature() {
+		return temperature;
+	}
+
+	public void setTemperature(String temperature) {
+		this.temperature = temperature;
+	}
+
+	public String getIdate() {
+		return idate;
+	}
+
+	public void setIdate(String idate) {
+		this.idate = idate;
+	}
+
+	public void setNum(String num) {
+		this.num = num;
+	}
+
+	public String getNum() {
+	return num;
+}
 }
