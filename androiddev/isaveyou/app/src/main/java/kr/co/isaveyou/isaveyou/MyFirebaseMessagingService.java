034cfef847c4d0bb41b7deed385119afcc742119
JK <great.joonki@gmail.com>
2018-03-17 17:28:29 +0900

    [Android] notification 수정

--- androiddev/isaveyou/app/src/main/java/kr/co/isaveyou/isaveyou/MyFirebaseMessagingService.java
+++ androiddev/isaveyou/app/src/main/java/kr/co/isaveyou/isaveyou/MyFirebaseMessagingService.java
@@ -3,47 +3,34 @@ package kr.co.isaveyou.isaveyou;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.Bundle;
-import android.os.IBinder;
 import android.os.PowerManager;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.RemoteViews;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.net.URL;
+
 import static android.content.Intent.ACTION_CALL;
-import static android.content.Intent.ACTION_DIAL;
 import static android.content.Intent.ACTION_VIEW;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "MyFirebaseMessaginService";
-
+    Bitmap bigPicture;
+    private static final String CALL_ACTION = "kr.co.isaveyou.CALL_ACTION";
+    private static final String CHECKPLACE_ACTION = "kr.co.isaveyou.CHECKPLACE_ACTION";
+    private static final String CHECK_EXT = "kr.co.isaveyou.CHECK_EXT_ACTION";
 
     //notification 활용을 위한 service - 푸시 메시지를 전달 받는 역할 담당
     //구글 클라우드 서버에서 보내오는 메시지를 받음
 
-
-//    View.OnClickListener handler = new View.OnClickListener() {
-//        @Override
-//        public void onClick(View v) {
-//            switch (v.getId()){
-//                case R.string.call
-//                    StartActivity
-//                    break;
-//            }
-//        }
-//    };
 
 
 
@@ -64,7 +51,13 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());}
-        sendNotification(remoteMessage.getData().get("title"), remoteMessage.getData().get("content"));
+
+        sendNotification(
+                //title, content, imgUrlLink를 인수로 지정
+                remoteMessage.getData().get("title"),
+                remoteMessage.getData().get("content"),
+                remoteMessage.getData().get("imgurllink")
+        );
 
 
         // 안드로이드 폰깨우기
@@ -77,7 +70,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         sCpuWakeLock.acquire();
 
-        if(sCpuWakeLock != null) { //acquire함수를 사용하였으면 꼭 release를 해줘야 cpu 메모리 점유 , 배터리 소모에 영향을 덜 준다/
+        if(sCpuWakeLock != null) { //acquire 함수를 사용하였으면 꼭 release를 해줘야 cpu 메모리 점유 , 배터리 소모에 영향을 덜 준다/
             sCpuWakeLock.release();
             sCpuWakeLock = null;
         }
@@ -86,13 +79,27 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     }
 
-    private void sendNotification(String title, String messageBody) {
+
+    private void sendNotification(String title, String messageBody, String myimgurl
+    ) {
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         Log.v(TAG, "notification 생성");
 
         int color = getResources().getColor(R.color.colorAccent);
+        //largeIcon에 이미지를 사용하기 위해서는 비트맵으로 바꿔줘야 함
         Bitmap mLargeIconForNoti = BitmapFactory.decodeResource(getResources(), R.drawable.pic_2nd);
+        //이미지 온라인 링크를 가져와 비트맵으로 바꿈
+        try {
+            URL url = new URL(myimgurl);
+            bigPicture = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
 
         String channelId = getString(R.string.default_notification_channel_id);
 
@@ -104,19 +111,32 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         Intent actionCall = new Intent(ACTION_CALL,Uri.parse("tel:119"));
         Intent actionCheckPlace = new Intent(ACTION_VIEW,Uri.parse("http://www.naver.com"));
-        Intent actionMonitoring = new Intent(ACTION_VIEW,Uri.parse("http://www.google.com"));
+//        Intent actionMonitoring = new Intent(ACTION_VIEW,Uri.parse("http://www.google.com"));
+        Intent actionCheckFire_ext = new Intent(ACTION_VIEW,Uri.parse("http://www.daum.net"));
+
 
 
         //pending intetn에 액션 추가
-        //FLAG_CANCEL_CURRENT : 이전에 생성한 PendingIntent 는 취소하고 새롭게 만든다.
+//        FLAG_CANCEL_CURRENT : 이전에 생성한 PendingIntent 는 취소하고 새롭게 만든다.
 //        FLAG_NO_CREATE : 생성된 PendingIntent 를 반환한다. 재사용 가능하다.
 //        FLAG_ONE_SHOT : 이 flag 로 생성한 PendingIntent 는 일회용이다.
 //        FLAG_UPDATE_CURRENT : 이미 생성된 PendingIntent 가 존재하면 해당 Intent 의 내용을 변경한다.
 
 
         PendingIntent callPendingIntent = PendingIntent.getActivity(getApplicationContext(),0,actionCall,PendingIntent.FLAG_CANCEL_CURRENT);
-        PendingIntent monitoringPendingIntent = PendingIntent.getActivity(getApplicationContext(),0,actionMonitoring,PendingIntent.FLAG_CANCEL_CURRENT);
+//        PendingIntent monitoringPendingIntent = PendingIntent.getActivity(getApplicationContext(),0,actionMonitoring,PendingIntent.FLAG_CANCEL_CURRENT);
         PendingIntent checkPlacePendingIntent = PendingIntent.getActivity(getApplicationContext(),0,actionCheckPlace,PendingIntent.FLAG_CANCEL_CURRENT);
+        PendingIntent checkFire_ext = PendingIntent.getActivity(getApplicationContext(),0,actionCheckFire_ext,PendingIntent.FLAG_CANCEL_CURRENT);
+
+//        RemoteViews contentView = new RemoteViews(getPackageName(),R.layout.notification_layout);
+//        contentView.setImageViewResource(R.id.monitoringImage, R.id.appImage);
+//        contentView.setTextViewText(R.id.tvTitle, title);
+//        contentView.setTextViewText(R.id.tvTitle, messageBody + "호 비상상황 발생, 빠르게 대피해주세요!");
+//        contentView.setOnClickPendingIntent(R.id.btn119,callPendingIntent);
+//        contentView.setOnClickPendingIntent(R.id.btnCheckPlace,checkPlacePendingIntent);
+//        contentView.setOnClickPendingIntent(R.id.btnMonitoring,monitoringPendingIntent);
+//        contentView.setOnClickPendingIntent(R.id.btnCheckFire_ext,checkFire_ext);
+
 
 
         NotificationCompat.Builder mNotificationBuilder = new NotificationCompat.Builder( this, channelId) //4.1 아래 버전과의 호환성을 위해 notificationCompat을 사용
@@ -124,27 +144,29 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 .setLargeIcon(mLargeIconForNoti) //큰 아이콘 세팅 - 큰 아이콘 세팅을 위해서 위에 bitmap 이용
                 .setColor(color)
                 .setDefaults(Notification.DEFAULT_LIGHTS|Notification.DEFAULT_SOUND)
-                .setContentTitle( title )
-                .setContentText(messageBody)
                 .setAutoCancel( false ) // notification을 클릭한 경우, notification을 사라지게 하려면 true 값을 줘야함
                 .setVibrate(pattern) //진동 설정
-
-                .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(mLargeIconForNoti))
-                .addAction(R.drawable.ic_call, getResources().getString(R.string.call), callPendingIntent) //119신고 액션 추가
-                .addAction(R.drawable.ic_stat_warnig, getResources().getString(R.string.checkPlace),checkPlacePendingIntent)
-                .addAction(R.drawable.ic_stat_monitoring, getResources().getString(R.string.checkPicture),monitoringPendingIntent) //화재 확인 액션 추가
                 .setPriority(NotificationCompat.PRIORITY_MAX)
                 .setSound(notificationSoundURI)
+                .setContentTitle(title)
+                .setWhen(System.currentTimeMillis())
+                .setStyle(new NotificationCompat.BigPictureStyle() /*스타일 지정*/
+                        .bigPicture(mLargeIconForNoti)
+                        .bigPicture(bigPicture)
+                        )
+                .setContentText(messageBody + "호 비상상황 발생, 빠르게 대피해주세요!")
+                .addAction(R.drawable.fire_call,getResources().getString(R.string.call),callPendingIntent) //119신고 액션 추가
+                .addAction(R.drawable.checkplace, getResources().getString(R.string.checkPlace),checkPlacePendingIntent)
+                .addAction(R.drawable.fire_ext,getResources().getString(R.string.checkFire_ext),checkFire_ext);
 
-                .setContentIntent(checkPlacePendingIntent)
-                .setContentIntent(callPendingIntent)
-                .setContentIntent(monitoringPendingIntent);
+
+//                .setContentIntent(checkPlacePendingIntent)
+//                .setContentIntent(callPendingIntent)
+//                .setContentIntent(monitoringPendingIntent);
 
 
 
 
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         notificationManager.notify(0 /* ID of notification */, mNotificationBuilder.build());
     }
