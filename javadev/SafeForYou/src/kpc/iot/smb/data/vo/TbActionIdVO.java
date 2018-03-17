7ac3791146095d02a3d8e30dad2809a8c73dec24
soulwawa <soulwawa85@gmail.com>
2018-03-17 16:10:51 +0900

    [SERVER] TbActionIdVO 추가

---
+++ javadev/SafeForYou/src/kpc/iot/smb/data/vo/TbActionIdVO.java
@@ -0,0 +1,36 @@
+package kpc.iot.smb.data.vo;
+
+import java.io.Serializable;
+
+public class TbActionIdVO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	int action_id;
+	String module_id;
+	String url;
+	String datetime;
+	public int getAction_id() {
+		return action_id;
+	}
+	public void setAction_id(int action_id) {
+		this.action_id = action_id;
+	}
+	public String getModule_id() {
+		return module_id;
+	}
+	public void setModule_id(String module_id) {
+		this.module_id = module_id;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getDatetime() {
+		return datetime;
+	}
+	public void setDatetime(String datetime) {
+		this.datetime = datetime;
+	}
+	
+}
