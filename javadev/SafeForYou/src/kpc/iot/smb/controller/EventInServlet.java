8ace33c8ea62127bcb85c5d10f899beb415694c1
soulwawa <soulwawa85@gmail.com>
2018-03-20 18:01:06 +0900

    [서버] 이벤트서블릿 편집

--- javadev/SafeForYou/src/kpc/iot/smb/controller/EventInServlet.java
+++ javadev/SafeForYou/src/kpc/iot/smb/controller/EventInServlet.java
@@ -19,19 +19,21 @@ public class EventInServlet extends Action{
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		response.setContentType("text/plain;charset=utf-8");
 //		PrintWriter out = response.getWriter();
+		String issue = request.getParameter("issue");
+		
 		String module_id = request.getParameter("module_id");
-		String temp1 = request.getParameter("temp");
-		float temp = toConvertTemp(temp1);
+		String temp = request.getParameter("temp");
+//		float temp = toConvertTemp(temp1);
 		String smoke = request.getParameter("smoke");
 		String gyro = request.getParameter("gyro");
 		String fire = request.getParameter("fire");
-		String issue = request.getParameter("issue");
+		
 		
 		EventDAO dao = new EventDAO();
 		TbEventVO vo = new TbEventVO();
 		
 		vo.setModule_id(module_id);
-		vo.setTemp(temp);
+		vo.setTemp(Float.parseFloat(temp));
 		vo.setSmoke(Float.parseFloat(smoke));
 		vo.setGyro(Float.parseFloat(gyro));
 		vo.setFire(Float.parseFloat(fire));
@@ -41,12 +43,12 @@ public class EventInServlet extends Action{
 		
 	}
 	//온도 변화
-	public float toConvertTemp(String reading){
-		 float readData = Float.parseFloat(reading);
-		 float voltage = (float) (readData * 5.0);
-		 voltage /= 1024.0;
-		 float temperatureC = (float) ((voltage - 0.5) * 100) ; 
-		 return (float) (Math.round(temperatureC * 100d) / 100d);
-	}
+//	public float toConvertTemp(String reading){
+//		 float readData = Float.parseFloat(reading);
+//		 float voltage = (float) (readData * 5.0);
+//		 voltage /= 1024.0;
+//		 float temperatureC = (float) ((voltage - 0.5) * 100) ; 
+//		 return (float) (Math.round(temperatureC * 100d) / 100d);
+//	}
 
 }
