22deece6ec4403402ebd831a12d9cb40e66d738b
soulwawa <soulwawa85@gmail.com>
2018-03-20 18:37:19 +0900

    [서버] 테스트입니다

--- javadev/SafeForYou/src/kpc/iot/smb/controller/WebLogin.java
+++ javadev/SafeForYou/src/kpc/iot/smb/controller/WebLogin.java
@@ -6,9 +6,6 @@ import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.google.gson.JsonObject;
-
 import kpc.iot.smb.data.dao.HrDAO;
 import kpc.iot.smb.data.vo.TbHrVO;
 import kpc.iot.smb.util.Action;
