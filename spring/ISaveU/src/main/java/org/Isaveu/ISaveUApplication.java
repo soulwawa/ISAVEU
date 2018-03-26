2302dd90446195db60ca43c8a927d0c65c817595
soulwawa <soulwawa85@gmail.com>
2018-03-26 10:39:11 +0900

    [서버] 스프링(부트) 프로젝트 생성

---
+++ spring/ISaveU/src/main/java/org/Isaveu/ISaveUApplication.java
@@ -0,0 +1,12 @@
+package org.Isaveu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ISaveUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ISaveUApplication.class, args);
+	}
+}
