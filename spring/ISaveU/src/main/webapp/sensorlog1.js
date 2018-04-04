4ab9cea1c3434f0b38db7f178ae2f3d34ec8e8cf
jeong geung hyul <rmdgbf156@naver.com>
2018-04-04 18:06:54 +0900

    admin chart

--- spring/ISaveU/src/main/webapp/sensorlog1.js
+++ spring/ISaveU/src/main/webapp/sensorlog1.js
@@ -39,7 +39,7 @@ var layoutStr6 = '<rMateChart backgroundColor="#FFFFFF"  borderStyle="none">'
 						+'<CategoryAxis categoryField="time" padding="0.2"/> '
 					+'</horizontalAxis>'
 					+'<verticalAxis>'
-						+'<LinearAxis minimum="25" maximum="30" interval="0.5"/>'
+						+'<LinearAxis minimum="28" maximum="30" interval="0.2"/>'
 					+'</verticalAxis>'
 					+'<series>'
 					
